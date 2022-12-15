d98fa9e5057d5c4d9d17e8af9689f3b9baf3894d
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-12-15 17:25:44 -0500

    init

---
+++ src/Transition_GraphicsManager_Patch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace InstantTransition
+{
+    [HarmonyPatch(typeof(GraphicsManager), nameof(GraphicsManager.SetLoading))]
+    public static class Transition_GraphicsManager_Patch
+    {
+
+        public static void Prefix(GraphicsManager __instance, ref float __result, ref bool __runOriginal, bool _Loading, ref bool ___LoadingScreen)
+        {
+            ___LoadingScreen = _Loading;
+            __instance.FadeToBlack.SetFade(FadeToBlackTypes.None, "", false);
+            __result = 0f;
+            __runOriginal = false;
+
+        }
+    }
+}
